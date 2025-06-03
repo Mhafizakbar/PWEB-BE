c00f8c11e3ac987bff9cbd260d28a9da1ae10cf8
Mhafizakbar <hafizakbar5707@gmail.com>
2025-06-03 15:35:08 +0700

    2

--- src/Control/user.ts
+++ src/Control/user.ts
@@ -13,9 +13,7 @@ pengguna.use(
   "*",
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://lomba-tif.vercel.app",
-      "https://lomba-tif.my.id",
+      "https://pweb-be-production.up.railway.app"
     ],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowHeaders: ["Authorization", "Content-Type"],
