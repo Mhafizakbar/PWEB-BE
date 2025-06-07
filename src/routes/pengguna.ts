0f4f15f3cd260caaf9ed9969c9240d13d7062764
Mhafizakbar <hafizakbar5707@gmail.com>
2025-06-07 23:47:21 +0700

    commit

--- src/routes/pengguna.ts
+++ src/routes/pengguna.ts
@@ -1,12 +1,12 @@
 import { Hono } from 'hono'
 import prisma from '../db'
 import { authMiddleware } from '../Middleware/authMiddleware'
-import { adminMiddleware } from '../Middleware/Admin'
+import Admin from '../Middleware/Admin'
 
 export const pengguna = new Hono()
 
 // hanya admin bisa akses data semua user
-pengguna.get('/pengguna', authMiddleware, adminMiddleware, async (c) => {
+pengguna.get('/pengguna', authMiddleware, Admin, async (c) => {
   const data = await prisma.pengguna.findMany({
     orderBy: { id_pengguna: 'asc' },
     select: {
